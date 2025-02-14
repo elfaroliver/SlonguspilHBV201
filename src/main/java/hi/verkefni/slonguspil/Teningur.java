ce27e0fc6ff7f2e878fae7bd56926f6fba02d24d
elfaroliver <eos35@hi.is>
2025-02-14 13:14:57 +0000

    Leikmadur búið að batna

--- src/main/java/hi/verkefni/slonguspil/Teningur.java
+++ src/main/java/hi/verkefni/slonguspil/Teningur.java
@@ -7,22 +7,27 @@ public class Teningur {
     private static final int MAX = 6;
     private final IntegerProperty talaProperty = new SimpleIntegerProperty(MAX);
     private int tala;
+    private int kast;
 
     public Teningur() {
         //IntegerProperty tala = new SimpleIntegerProperty((6) + 1);
-        this.tala = 6;
+        //this.tala = 6;
     }
 
     public String toString() {
-        return "Kast: " + tala;
+        return "Kast: " + kast;
     }
 
     public void kasta() {
         //new Teningur();
         //System.out.println(new Teningur());
 
-        this.tala = (int)(Math.random() * 6) + 1;
+        kast = (int)(Math.random() * MAX) + 1;
         System.out.println(this);
+    }
+
+    public int getKast() {
+        return kast;
     }
 
     public static void main(String[] args) {
